e987eefd2e95f54856f50b4d6da587d57bd61683
Ilya Shirokov <ilyashirokov32@gmail.com>
2026-07-30 00:26:44 +0900

    hope fixed

--- database/crud.py
+++ database/crud.py
@@ -31,6 +31,7 @@ async def get_user_from_db(user_name: str) -> User:
 async def add_user_to_db(user_id: int, username: str, password: str, couple_id: int = None):
     async with session() as sess:
         try:
+            password = password.get_secret_value()
             hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
             new_user = User(id=user_id, username=username, couple_id=couple_id, password=hashed_password)
             sess.add(new_user)
